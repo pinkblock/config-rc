3dc4bc640c1172927c6c86c1a196d850baa74f28
semiller <semiller@lexmark.com>
2015-07-02 12:35:15 -0400

    EVERY THING WORKSgit add -A!

--- deploy_configs.sh
+++ deploy_configs.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
 ln -s ~/config-rc/.bashrc ~/.bashrc
-ln -s ~/config-rc/.vimrc ~/.vimrc
-ln -s ~/config-rc/.Xmodmap ~/.Xmodmap
+# ln -s ~/config-rc/.vimrc ~/.vimrc
 ln -s ~/config-rc/test.txt ~/Desktop/test.txt
+
+sh set_xmod.sh
+
+
+
