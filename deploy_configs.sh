7a794122532abb65a388400ab44bc3864821c631
Seith Miller <seithmiller@gmail.com>
2016-04-18 23:34:15 -0400

    added kbd.conf

--- deploy_configs.sh
+++ deploy_configs.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# bash rc
 ln -s ~/config-rc/.bashrc ~/.bashrc
+
 # ln -s ~/config-rc/.vimrc ~/.vimrc
+
+# put the test file on the desktop
 ln -s ~/config-rc/test.txt ~/Desktop/test.txt
 
+# put in new xkb.conf
+ln -s ~/config-rc/90-custom-kbd.conf /etc/X11/xorg.conf.d90-custom-kbd.conf
+
+
+# run set_xmod
 sh set_xmod.sh
 
 
